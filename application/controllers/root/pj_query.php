5937029b5ac4adb5bcbfed10b7b241229453ffb1
jefff8 <360531371@qq.com>
2017-07-26 19:53:06 +0800

    2017年7月26日19:52:59

---
+++ application/controllers/root/pj_query.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');	
+class Pj_query extends CI_Controller{
+	/**
+	 * 查询页面
+	 */
+	public function index(){
+		$data['pj_timestamp'] = $this->uri->segment(3);
+		$this->load->view('root/pj_query.html',$data);
+	}
+
+	/**
+	 * 执行查询
+	 */
+	public function query(){
+		$data = $this->input->post('type_data');
+		$this->load->model('root/pj_query','query');
+		$data['total'] = $this->query->query($data);
+		p($data['total']);
+	}
+}
+?>
