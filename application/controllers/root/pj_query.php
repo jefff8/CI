810b1c0cd7057db7d89f517f6e7123a69708972e
DT-Kim <398546027@qq.com>
2017-07-28 13:39:43 +0800

    2017-07-28

--- application/controllers/root/pj_query.php
+++ application/controllers/root/pj_query.php
@@ -7,16 +7,20 @@ class Pj_query extends CI_Controller{
 	public function index(){
 		$data['pj_timestamp'] = $this->uri->segment(3);
 		$this->load->view('root/pj_query.html',$data);
+//		$this->output->enable_profiler(TRUE);
 	}
 
 	/**
 	 * 执行查询
 	 */
 	public function query(){
-		$data = $this->input->post('type_data');
-		// $this->load->model('root/pj_query','check');
-		// $data['total'] = $this->check->query($data);
-		var_dump($data);
+//		$type = '材料送检';
+		$type = $this->input->post('type_data');
+		$this ->load->model('root/pj_query_model');
+		$data['total'] = $this->pj_query_model->select($type);
+		$json = json_encode($data);
+		echo $json;
+		
 	}
 }
 ?>
