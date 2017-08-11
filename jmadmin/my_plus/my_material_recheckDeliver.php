6d4a9243649a0d1e2adb19ccf1cbd31b04ab8a44
jefff8 <360531371@qq.com>
2017-08-11 13:04:08 +0800

    2017年8月11日13:04:05

--- jmadmin/my_plus/my_material_recheckDeliver.php
+++ jmadmin/my_plus/my_material_recheckDeliver.php
@@ -8,14 +8,12 @@
 	$shydw=$_POST['shydw'];
 	$syr=$_POST['syr'];
 	$jzr=$_POST['jzr'];
-	$ypbh=$_POST['ypbh'];
 	$second_num=$_POST['second_num'];
-	$testNum=$_POST['testNum'];
 	$sql = "select * from 材料送检  where id='".$id."'";
 	$result = $conn->query($sql);
 	$count = mysqli_num_rows($result);
 	if($result->num_rows>0){
-		$sqli = "update 材料送检  set 工程单状态='收样复检',收样日期='".$soyrq."',送样日期='".$shyrq."',送样单位='".$sydw."',见证单位='".$jzdw."',收样单位='".$shydw."',送样人='".$syr."',见证人='".$jzr."',样品编号='".$ypbh."',复检编号='".$second_num."',检测报告编号='".$testNum."' where id='".$id."'";
+		$sqli = "update 材料送检  set 工程单状态='收样复检',收样日期='".$soyrq."',送样日期='".$shyrq."',送样单位='".$sydw."',见证单位='".$jzdw."',收样单位='".$shydw."',送样人='".$syr."',见证人='".$jzr."',样品编号='".$second_num."' where id='".$id."'";
 		if($conn->query($sqli) === TRUE){
 			$jsonresult = 'success';
 		}else{
