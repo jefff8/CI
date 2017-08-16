ecad6d4b141bb73e26242acda1cd2802f9673264
jefff8 <360531371@qq.com>
2017-08-16 18:12:32 +0800

    2017年8月16日18:12:29

--- jmadmin/my_task/self_inspection1.php
+++ jmadmin/my_task/self_inspection1.php
@@ -15,6 +15,32 @@
 			echo $data_json;
 			$conn->close();
 			break;
+		case "获取时间戳":
+			$sql = "select 时间戳  from 材料自检  where  id = '$ulId' " ;
+			$result = $conn->query($sql);
+			if($result->num_rows >0){
+				while($row = $result->fetch_assoc()){
+					$data_arr['时间戳']=$row['时间戳'];
+				}
+			}
+			$data_json = json_encode($data_arr);
+			echo $data_json;
+			$conn->close();
+			break;
+		case "单位名称":
+			$pj_time = $_POST['pj_timestamp'];
+			$sql = "select 施工单位,检测单位,监理单位 from 我的工程  where 时间戳='$pj_time'";
+			$result = $conn->query($sql);
+			if($result){
+				$row = $result->fetch_assoc();
+				$data[0]['施工单位'] = $row['施工单位'];
+				$data[0]['检测单位'] = $row['检测单位'];
+				$data[0]['监理单位'] = $row['监理单位'];
+			}
+			$conn->close();
+			$data = json_encode($data);
+			echo $data;
+			break;
 		case "删除":
 			$sql = "delete from 材料自检  where id='$ulId'";
 			$result = $conn->query($sql);
@@ -58,6 +84,18 @@
 				$return['result'] = "撤销成功";
 			}else{
 				$return['result'] = "撤销失败";
+			}
+			$json = json_encode($return);
+			echo $json;
+			$conn->close();
+			break;
+		case "合格":
+			$sql = "update 材料自检 set 工程单状态='合格' where id='$ulId'";
+			$result = $conn->query($sql);
+			if($result){
+				$return['result'] = "操作成功";
+			}else{
+				$return['result'] = "操作失败";
 			}
 			$json = json_encode($return);
 			echo $json;
