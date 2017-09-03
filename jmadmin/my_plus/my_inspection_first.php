1c55d2def4d71b33a5a903f98701760f8fa3424c
笨鸟先生 <30309626+1345739670@users.noreply.github.com>
2017-09-03 15:08:32 +0800

    2017.9.3

---
+++ jmadmin/my_plus/my_inspection_first.php
@@ -0,0 +1,61 @@
+<?php
+	$flag = $_POST['flag'];
+	
+	if($flag == "first" ){
+		$timestamp = $_POST['sjc'];
+		$project_id = $_POST['gcid'];
+		require("../conn.php");
+		$sql = "select  *  from 材料自检初检表   where id='$project_id' and 时间戳='$timestamp'";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				$return_data['工程单状态']=$row['工程单状态'];
+				$return_data['自检自测类型']=$row['自检自测类型'];
+				$return_data['检测部位']=$row['检测部位'];
+				$return_data['检测数量']=$row['数量'];
+				$return_data['检测人']=$row['检测人'];
+				$return_data['检测日期']=$row['检测日期'];
+				$return_data['检测单位']=$row['检测单位'];
+				$return_data['备注']=$row['备注'];
+				$return_data['送样日期']=$row['送样日期'];
+				$return_data['收样日期']=$row['收样日期'];
+				$return_data['送样单位']=$row['送样单位'];
+				$return_data['收样单位']=$row['收样单位'];
+				$return_data['见证单位']=$row['见证单位'];
+				$return_data['见证人']=$row['见证人'];
+				$return_data['处理情况']=$row['处理情况'];
+				$return_data['退场记录']=$row['退场记录'];
+				$return_data['场景照片说明']=$row['场景照片说明'];
+				$return_data['场景照片说明']=$row['场景照片说明'];
+				$return_data['场景照片说明']=$row['场景照片说明'];
+				$return_data['场景照片说明']=$row['场景照片说明'];
+			}
+		}
+	}else if($flag == "获取图片"){
+		$timestamp = $_POST['sjc'];
+		$project_id = $_POST['gcid'];
+		require("../conn.php");
+		$sql = "select * from 材料自检初检表   where id='$project_id' and 时间戳='$timestamp'";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				$return_data['时间戳']=$row['时间戳'];
+				$return_data['工程单状态']=$row['工程单状态'];
+				$return_data['检测前照片']=$row['检测前照片'];
+				$return_data['检测实施过程照片']=$row['检测实施过程照片'];
+				$return_data['检测设备照片']=$row['检测设备照片'];
+				$return_data['自测照片']=$row['自测照片'];
+				$return_data['处理照片']=$row['处理照片'];
+				$return_data['场景照片说明']=$row['场景照片说明'];
+				$return_data['检测实施过程照片说明']=$row['检测实施过程照片说明'];
+				$return_data['检测设备照片说明']=$row['检测设备照片说明'];
+				$return_data['自测照片说明']=$row['自测照片说明'];
+				$return_data['处理照片说明']=$row['处理照片说明'];
+			}
+		}
+	}
+	$json = json_encode($return_data);
+	echo $json;
+	$conn->close();
+	
+?>
