cefc02ef31aa484b5c8387b1411eea81951e9521
jefff8 <360531371@qq.com>
2017-09-07 13:15:08 +0800

    2017年9月7日13:15:03

--- application/controllers/root/commission.php
+++ application/controllers/root/commission.php
@@ -7,6 +7,7 @@
 			$this->load->model('root/commission_model','commission');
 			$data['commission_data'] = $this->commission->commission($commission_timestamp);
 			$data['witness_data'] = $this->commission->witness($commission_timestamp);
+			$data['test'] = $this->commission->test($commission_timestamp);
 			$data['result_data'] = $this->commission->result($commission_timestamp);
 			$data['commission_timestamp'] = $commission_timestamp;
 			$this->load->view('root/commission.html',$data);
@@ -24,7 +25,59 @@
 			$data['pj_timestamp'] = $pj_timestamp;
 			$this->load->view('root/commission_Detail.html',$data);
 		}
+
+		/**
+		 * 合格
+		 */
+		public function qualified(){
+			$pj_timestamp = $this->input->post('pj_timestamp');
+			$data['self_id'] = $this->input->post('self_id');
+			$data['testNum'] =  $this->input->post('testNum');
+			$data['pj_status'] = $this->input->post('pj_status');
+			$this->load->model('root/Commission_model','qualified');
+			$this->qualified->qualified($data);
+	    }
 		
-		
+		/**
+		 * 文件上传
+		 */
+		public function do_upload(){
+			$imgs = $_FILES;
+			$data['result'] =  $this->input->post('result');
+			$data['self_id'] = $this->input->post('self_id');
+			$data['testNum'] =  $this->input->post('testNum');
+			$data['explain'] =  $this->input->post('explain');
+			$pj_timestamp = $this->input->post('pj_timestamp');
+			//上传配置  
+			$config['upload_path'] = './jmadmin/upload/';
+	        $config['allowed_types'] = 'gif|jpg|png';
+	        $config['max_size'] = 0;
+	        $config['max_width'] = 0;
+	        $config['max_height'] = 0;
+	        $config['file_name']  = time().'.jpg';
+	        $data['filename'] = "~*^*~".$config['file_name'];
+	        //加载上传类 
+	        $this->load->library('upload', $config);
+	        //执行上传任务
+	        if (!$this->upload->do_upload('userfile'))
+	        {
+	        	//上传失败
+	        	$error = $this->upload->display_errors();
+	            echo "<script>alert('$error');window.history.back();</script>";
+	            die;
+	        }
+	        else
+	        {
+	         //    $data['upload_data']=$this->upload->data();  //文件的一些信息
+		        // $img=$data['upload_data']['file_name'];  //取得文件名
+		        // echo $img."<br>";
+		        // foreach ($data['upload_data'] as $item => $value){ 
+		        //      echo $item . ":" . $value . "<br>";    
+		        // }
+		        $this->load->model('root/Commission_model','fail');
+		        $this->fail->fail($data);
+		        success('Commission/index/'.$pj_timestamp,"操作成功！");
+	        }
+		}
 	}
 ?>
--- application/models/root/commission_model.php
+++ application/models/root/commission_model.php
@@ -15,6 +15,11 @@ class Commission_model extends CI_Model{
 		$data = $this->db->query($sql,array($commission_timestamp,'待确认','已确认'))->result_array();
 		return $data;
 	}
+	public function test($commission_timestamp){
+		$sql = "select * from 实体检测 where 工程时间戳=? and 工程单状态=? ";
+		$data = $this->db->query($sql,array($commission_timestamp,'提交结果'))->result_array();
+		return $data;
+	}
 	public function result($commission_timestamp){
 		$sql = "select * from 实体检测 where 工程时间戳=? and (工程单状态=? or 工程单状态=? or 工程单状态=?)";
 		$data = $this->db->query($sql,array($commission_timestamp,'合格','不合格','已处理'))->result_array();
@@ -26,5 +31,29 @@ class Commission_model extends CI_Model{
 		$data = $this->db->query($sql,array($com_id))->result_array();
 		return $data;
 	}
-}
+	/**
+	 * 合格
+	 */
+	public function qualified($data){
+		$testNum = $data['testNum'];
+		$id = $data['self_id'];
+		$pj_status = $data['pj_status'];
+		$info = array('工程单状态'=>'合格','检测报告编号'=>$testNum);
+		$where = "id = '$id'";
+		$this->db->update('实体检测',$info,$where);
+	}
+	/**
+	 * 不合格
+	 */
+	public function fail($data){
+		$img = $data['filename'];
+		$result = $data['result'];
+		$id = $data['self_id'];
+		$testNum = $data['testNum'];
+		$explain = $data['explain'];
+		$info = array('工程单状态'=>'不合格','不合格报表'=>$img,'检测报告编号'=>$testNum,'报告照片说明'=>$explain,'不合格报告'=>$result);
+		$where = "id = '$id'";
+		$this->db->update('实体检测',$info,$where);
+	}
+}	
 ?>
