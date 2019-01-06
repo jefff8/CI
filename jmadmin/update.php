f3898f8d65c61b3bc16ee55a1e9086da66840ca1
jeff8 <360531371@qq.com>
2019-01-06 16:13:56 +0800

    2019年1月6日16:13:53

--- jmadmin/update.php
+++ jmadmin/update.php
@@ -1,6 +1,6 @@
 <?php
 	$appid="H5986FCE1";
-	$version="2018.02.0730";	//app版本号
+	$version="2018.02.1020";	//app版本号
 	$note="江门市建设工程施工质量管理系统";	//app描述
 	//$appurl="http://192.168.155.1:80/hxajxt/release/hxajxt.apk";	//app下载地址
 	$appurl="http://182.61.39.167/jmConstr/jmadmin/release/jmConstr.apk";
